66acaada8fbafb800e48d72d1c078a25dbee268f
Filip <fseilvicpik@gmail.com>
2025-03-26 14:56:41 +0100

    labs are working

---
+++ src/keyboard_control_node.cpp
@@ -0,0 +1,12 @@
+
+#include "../include/KeyBoardControl.hpp"
+
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<KeyboardControlNode>());
+    rclcpp::shutdown();
+    return 0;
+
+ }
